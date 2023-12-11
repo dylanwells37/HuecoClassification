d4436071b7ef8ddca7c49e08b0c2b1d337359588
Dylan Wells <105832162+dylanwells37@users.noreply.github.com>
2023-12-10 23:43:01 -0500

    Gentic algorithm test

--- GeneticAlgorithmTuning/GA.py
+++ GeneticAlgorithmTuning/GA.py
@@ -1,25 +1,314 @@
 import numpy as np
-import network as ga_nn
+import pandas as pd
+import pickle
+import time
+
+from copy import deepcopy
+from pathlib import Path
+from network import gaNetwork
 
 class GA:
-    
-    
-    def __init__(self, n_population, n_generations, n_crossover, n_mutate, n_layers):
-        self.n_population = n_population
-        self.n_generations = n_generations
+    def __init__(self, n_population, n_crossover, n_mutate,
+                 input_shape, output_shape):
+        self.n_pop = n_population
         self.n_crossover = n_crossover
         self.n_mutate = n_mutate
+        self.n_layers = 8
+        self.input_shape = input_shape
+        self.output_shape = output_shape
     
         # define the possible values for each parameter
         self.parameters = {
-            'n_layers': [1, 2, 3, 4, 5, 6, 7, 8],
+            #'n_layers': [1, 2, 3, 4, 5, 6, 7, 8],
             'n_neurons': [8, 16, 32, 64, 128, 256, 512],
             'activation': ['relu', 'sigmoid', 'tanh', 'softmax'],
             'optimizer': ['adam', 'rmsprop', 'sgd'],
             'loss': ['binary_crossentropy', 'categorical_crossentropy']
         }     
-
+        self.population = []
+        self.fitnesses = []
+        self.best_fitness = 0
+        self.best_network = None
+        self.best_history = None
+        self.best_model = None
+        # count the number of genes in the genome
+        self.gene_count = self.n_layers * 2 + 2
+        self.genfitnesses = []
         
     def initialize_population(self):
-        for i in range(self.n_population):
+        for i in range(self.n_pop):
+            #n_layers = np.random.choice(self.parameters['n_layers'])
+            n_layers = self.n_layers
+            n_neurons = [np.random.choice(self.parameters['n_neurons']) 
+                         for j in range(n_layers)]
+            activation = [np.random.choice(self.parameters['activation']) 
+                          for j in range(n_layers)]
+            optimizer = np.random.choice(self.parameters['optimizer'])
+            loss = np.random.choice(self.parameters['loss'])
+            id = i
             
+            self.population.append(gaNetwork(n_layers, n_neurons, 
+                                             activation, optimizer, loss, id,
+                                             self.input_shape, self.output_shape))
+    
+    
+    def train_population(self, x_train, y_train, epochs, batch_size, 
+                         validation_data, verbose=0):
+        for network in self.population:
+            if verbose == 1:
+                time_start = time.time()
+                print(f"Training network {network.id}...")
+            network.build_model()
+            network.train_model(x_train, y_train, epochs, 
+                                batch_size, validation_data)
+            if verbose == 1:
+                time_end = time.time()
+                print(f"Network {network.id} trained in {time_end - time_start} seconds")
+    
+    
+    def evaluate_population(self):
+        fit_list = []
+        for network in self.population:
+            fitness = network.get_best_val_acc()
+            fit_list.append(fitness)
+        self.fitnesses = fit_list
+        self.genfitnesses.append(np.max(fit_list))
+        
+        with open('tracker.txt', 'a') as f:
+            f.write(f"{np.max(fit_list)}\n")
+
+        if np.max(fit_list) > self.best_fitness:
+            self.best_fitness = np.max(fit_list)
+            self.best_network = deepcopy(self.population[np.argmax(fit_list)])
+            self.best_history = self.best_network.get_history()
+            self.best_model = self.best_network.get_model()
+            # save the best model
+            self.best_model.save('best_model.h5')
+            with open('best_txt.txt', 'wb') as f:
+                # Write the genes of the best network to a file
+                genes = self.best_network.get_genes()
+                for gene in genes:
+                    f.write(f"{gene}\n")
+        
+    
+    def select_parents(self):
+        parent_no = self.n_crossover * 2 + self.n_mutate
+        roulette_no = int(parent_no * (1/4))
+        rank_no = int(parent_no * (1/2))
+        tournament_no = parent_no - roulette_no - rank_no
+        
+        parent_ids = []
+        
+        parent_ids.append(self.roulette_selection(roulette_no))
+        parent_ids.append(self.rank_selection(rank_no))
+        parent_ids.append(self.tournament_selection(tournament_no))
+            
+        return parent_ids
+        
+    
+    def torunament_selection(self, tournament_no):
+        # randomly select 7 percent of the population and select the best
+        # individual from that group
+        parent_ids = []
+        for i in range(tournament_no):
+            tournament = np.random.choice(self.population, int(self.n_pop * 0.07))
+            fitnesses = [network.get_best_val_acc() for network in tournament]
+            parent_ids.append(tournament[np.argmax(fitnesses)].id)
+            
+        return parent_ids
+        
+    
+    def roulette_selection(self, roulette_no):
+        
+        parent_ids = []
+        fitnesses = np.array(self.fitnesses)
+        fitnesses = fitnesses / np.sum(fitnesses)
+        
+        for i in range(roulette_no):
+            parent_ids.append(np.random.choice(self.population, p=fitnesses).id)
+        
+        return parent_ids
+    
+    
+    def rank_selection(self, rank_no):
+        parent_ids = []
+        fitnesses = np.array(self.fitnesses)
+        indices = np.argsort(fitnesses)
+        p_array = np.arange(1, len(indices) + 1) / np.sum(np.arange(1, len(indices) + 1))
+        
+        for i in range(rank_no):
+            index = np.random.choice(indices, p=p_array)
+            parent_ids.append(self.population[index].id)
+        
+        return parent_ids
+            
+    
+    def crossover(self, parent_ids):
+        # randomly select two parents and perform crossover
+        # repeat until the desired number of offspring is reached
+        offspring = []
+        
+        for i in range(self.n_crossover):
+            # check if the set of parent_ids has only one element,
+            # then we can't perform crossover
+            set_ids = set(parent_ids)
+            if len(set_ids) == 1:
+                return offspring, parent_ids, self.n_crossover - i
+            
+            parent1 = np.random.choice(parent_ids)
+            parent2 = np.random.choice(parent_ids)
+            
+            while parent1 == parent2:
+                parent2 = np.random.choice(parent_ids)
+
+                
+            # choose a gene to swap
+            chosen_gene = np.random.randint(0, self.gene_count)
+            gene1 = self.population[parent1].get_gene(chosen_gene)
+            gene2 = self.population[parent2].get_gene(chosen_gene)
+            child1 = deepcopy(self.population[parent1])
+            child2 = deepcopy(self.population[parent2])
+            child1.set_gene(chosen_gene, gene2)
+            child2.set_gene(chosen_gene, gene1)
+            
+            offspring += [child1, child2]
+                
+            # remove the parents from the list of ids
+            parent_ids.pop(parent_ids.index(parent1))
+            parent_ids.pop(parent_ids.index(parent2))
+        
+        return offspring, parent_ids, 0
+    
+    
+    def mutation(self, parent_ids, extra_parents):
+        # randomly select a parent and mutate it
+        # repeat until the desired number of offspring is reached
+        offspring = []
+        
+        for i in range(self.n_mutate + extra_parents):
+            parent = np.random.choice(parent_ids)
+            
+            child = deepcopy(self.population[parent])
+            
+            # choose a gene to mutate
+            chosen_gene = np.random.randint(0, self.gene_count)
+            if chosen_gene < self.n_layers:
+                gene_set = set(self.parameters['n_neurons']) - {child.get_gene(chosen_gene)} 
+            elif chosen_gene < self.n_layers * 2:
+                gene_set = set(self.parameters['activation']) - {child.get_gene(chosen_gene)}
+            elif chosen_gene == self.n_layers * 2:
+                gene_set = set(self.parameters['optimizer']) - {child.get_gene(chosen_gene)}
+            elif chosen_gene == self.n_layers * 2 + 1:
+                gene_set = set(self.parameters['loss']) - {child.get_gene(chosen_gene)}
+            else:
+                print("Error: gene index out of range")
+                
+            gene = np.random.choice(list(gene_set))
+            child.set_gene(chosen_gene, gene)
+            
+            offspring.append(child)
+            parent_ids.pop(parent_ids.index(parent))
+            
+        return offspring, parent_ids
+        
+        
+    def next_generation(self):
+        parent_ids = self.select_parents()
+        offspring = []
+        offspring, parent_ids, extra_parents = self.crossover(parent_ids)
+        offspring, parent_ids = self.mutation(parent_ids, extra_parents)
+        
+        # if the number of offspring is less than the desired number,
+        # then we need to create more offspring
+        difference = self.n_pop - len(offspring)
+        new_ids = self.torunament_selection(difference)
+        if len(new_ids) > 0:
+            offspring += [self.population[id] for id in new_ids]
+            
+        for i in range(self.n_pop):
+            offspring[i].id = i
+            
+        self.population = offspring
+    
+    
+    def get_best_network(self):
+        return self.best_network
+    
+    def get_best_history(self):
+        return self.best_history
+    
+    def get_best_model(self):
+        return self.best_model
+    
+    def get_best_fitness(self):
+        return self.best_fitness
+    
+    def get_genfitnesses(self):
+        return self.genfitnesses
+
+
+# main
+if __name__ == "__main__":
+    # load the dataset
+    # Load data
+    # Load data
+    cwd = Path.cwd()
+    moondf = pickle.load(open(cwd / '..' / 'raw_data' / 'moonGen_scrape_2016_with_labels', 'rb'))
+    # change the grade column from a number 4 - 14 to a list of 11 binary values
+    y_temp = moondf['grade'].values
+    moondf['grade'] = moondf['grade'].apply(lambda x: [1 if i == x else 0 for i in range(4, 15)])
+    # one hot encode the grade column
+    grade_cols = ['V_' + str(i) for i in range(4, 15)]
+    moondf[grade_cols] = pd.DataFrame(moondf['grade'].to_list(), index=moondf.index)
+    y = moondf[grade_cols].values
+    X = moondf.drop(columns=grade_cols, axis=1)
+    X = X.drop(["is_benchmark", "repeats", "grade"], axis=1).values
+    
+
+    X_train = np.zeros((0, 141))
+    y_train = np.zeros((0, 11))
+    X_rest = np.zeros((0, 141))
+    y_rest = np.zeros((0, 11))
+    sample_size = 2400
+    for i in range(4, 15):
+        if len(X[y_temp == i])*0.8 < sample_size:
+            temp_size = int(len(X[y_temp == i]) * 0.8)
+        else:
+            temp_size = sample_size
+        X_train = np.concatenate((X_train, X[y_temp == i][:temp_size]))
+        y_train = np.concatenate((y_train, y[y_temp == i][:temp_size]))
+        # remove those from the X and y
+        X_rest = np.concatenate((X_rest, X[y_temp == i][temp_size:]))
+        y_rest = np.concatenate((y_rest, y[y_temp == i][temp_size:]))
+
+
+
+    genetic_algorithm = GA(100, 80, 20, (141,), 11)
+    
+    generation = 0
+    stop_gen = 1000
+    print("Initializing population")
+    genetic_algorithm.initialize_population()
+    
+    while generation < stop_gen:
+        print(f"Generation {generation}")
+        start = time.time()
+        print("Training population")
+        genetic_algorithm.train_population(X_train, y_train, 50, 16, 
+                                           (X_rest, y_rest))
+        print("Evaluating population")
+        genetic_algorithm.evaluate_population()
+        print("Creating next generation")
+        genetic_algorithm.next_generation()
+        
+        generation += 1
+        end = time.time()
+        
+        print("Best fitness: ", genetic_algorithm.get_best_fitness())
+        print(f"Generation {generation} complete in {end - start} seconds")
+        
+
+
+    
+        
+    
--- GeneticAlgorithmTuning/network.py
+++ GeneticAlgorithmTuning/network.py
@@ -3,29 +3,97 @@ import keras
 
 class gaNetwork:
     
-    def __init__(self, n_layers, n_neurons, activation, optimizer, loss):
-        self.n_layers = n_layers # number of layers
-        self.n_neurons = n_neurons # array of number of neurons in each layer
-        self.activation = activation # array of activation functions for each layer
-        self.optimizer = optimizer # optimizer
-        self.loss = loss # loss function
-        self.model = None # keras model
+    def __init__(self, n_layers, n_neurons, activation, optimizer, loss, id,
+                 input_shape, output_shape):
+        self.id = id 
+        self.n_layers = n_layers 
+        self.n_neurons = n_neurons 
+        self.activation = activation 
+        self.optimizer = optimizer 
+        self.loss = loss 
+        self.model = None 
+        self.history = None
+        self.best_val_acc = None
+        self.input_shape = input_shape
+        self.output_shape = output_shape
         
-    def build_model(self, input_shape, output_shape):
+    def build_model(self):
         model = keras.models.Sequential()
         for i in range(self.n_layers):
             if i == 0:
-                model.add(keras.layers.Dense(self.n_neurons[i], activation=self.activation[i], input_shape=input_shape))
+                model.add(keras.layers.Dense(self.n_neurons[i],
+                                             activation=self.activation[i], 
+                                             input_shape=self.input_shape))
             else:
-                model.add(keras.layers.Dense(self.n_neurons[i], activation=self.activation[i]))
-        model.add(keras.layers.Dense(output_shape, activation='softmax'))
-        model.compile(optimizer=self.optimizer, loss=self.loss)
+                model.add(keras.layers.Dense(self.n_neurons[i],
+                                             activation=self.activation[i]))
+        model.add(keras.layers.Dense(self.output_shape, activation='softmax'))
+        model.compile(optimizer=self.optimizer, loss=self.loss, metrics=['accuracy'])
         self.model = model
     
         
-    def train_model(self, x_train, y_train, epochs, batch_size):
+    def train_model(self, x_train, y_train, 
+                    epochs, batch_size, 
+                    validation_data, verbose=1):
         
-        self.model.fit(x_train, y_train, epochs=epochs, batch_size=batch_size
-                          , verbose=0)
+        callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', patience=3),
+                    keras.callbacks.ModelCheckpoint(filepath=f'generation_models/best_model_{self.id}.h5', 
+                                                     monitor='loss', 
+                                                     save_best_only=True)]
         
+        self.model.fit(x_train, y_train, epochs=epochs, 
+                       batch_size=batch_size, verbose=verbose,
+                       validation_data=validation_data, 
+                       callbacks=callbacks)
+
+        self.history = self.model.history.history
+        
+        # print the keys of the history dictionary
+        print(self.history.keys())
+        
+        self.best_val_acc = np.max(self.history['val_accuracy'])
+    
+    
+    def get_model(self):
+        return self.model
+    
+    
+    def get_history(self):
+        return self.history
+    
+    
+    def get_best_val_acc(self):
+        return self.best_val_acc
+
+
+    def get_gene(self, index):
+        if index < self.n_layers:
+            return self.n_neurons[index]
+        elif index < self.n_layers * 2:
+            return self.activation[index - self.n_layers]
+        elif index == self.n_layers * 2:
+            return self.optimizer
+        elif index == self.n_layers * 2 + 1:
+            return self.loss
+
+        print("Error: index out of range")
+        return None
+    
+    
+    def set_gene(self, index, value):
+        if index < self.n_layers:
+            self.n_neurons[index] = value
+        elif index < self.n_layers * 2:
+            self.activation[index - self.n_layers] = value
+        elif index == self.n_layers * 2:
+            self.optimizer = value
+        elif index == self.n_layers * 2 + 1:
+            self.loss = value
+        else:
+            print("Error: index out of range")
+            return None
+        
+    
+    def get_genes(self):
+        return self.n_neurons + self.activation + [self.optimizer, self.loss]
     
